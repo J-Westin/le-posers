Fixed saving of examples automatically scaling maximum color value to maximum in image in pose_cgan_2.py.

pose_cgan_2.py
@@ -18,6 +18,7 @@ from keras.utils.vis_utils import plot_model
 
 
 from PIL import Image
+import cv2
 
 import numpy as np
 import os
@@ -67,11 +68,12 @@ class DiscriminatorDataGenerator(Sequence):
 	def __init__(self, label_list, speed_root,
 				batch_size=32, dim=(1920, 1200,1),
 				shuffle=True, seed = 1, real_p = 0.5,
-				generator = None, latent_dim = None, hidden_dim = None):
+				generator = None, latent_dim = None, hidden_dim = None,
+				clip_range = None):
 
 		# loading dataset
 		self.image_root = os.path.join(speed_root, 'images', 'train')
-
+		self.clip_range=clip_range
 		# Initialization
 		self.dim = dim
 		self.real_p = real_p
@@ -144,7 +146,8 @@ class DiscriminatorDataGenerator(Sequence):
 	
 	
 				# flatten and output
-				x = np.expand_dims(keras_image.img_to_array(img)[...,0]/255.,2)
+#				np.expand_dims(keras_image.img_to_array(img)[...,0]/255.,2)
+				x = np.expand_dims((np.clip(keras_image.img_to_array(img)[...,0],self.clip_range[0],self.clip_range[1])-self.clip_range[0])/(-self.clip_range[0]+self.clip_range[1]),2)
 				pose = np.concatenate([q, r])
 			else:
 #				noise = np.random.rand(self.latent_dim).reshape(1,self.latent_dim)
@@ -181,10 +184,11 @@ class GeneratorDataGenerator(Sequence):
 	def __init__(self, label_list, speed_root,
 				batch_size=32, dim=(1200, 1920),
 				shuffle=True, seed = 1, real_p = 0.5,
-				generator = None, latent_dim = None):
+				generator = None, latent_dim = None, clip_range = None):
 
 		# loading dataset
 		self.image_root = os.path.join(speed_root, 'images', 'train')
+		self.clip_range=clip_range
 
 		# Initialization
 		self.dim = dim
@@ -265,11 +269,14 @@ class GeneratorDataGenerator(Sequence):
 class CGAN(object):
 
 	def __init__(self, batch_size, epochs, version, load_model, reps = 4):
+		
 		#### tweakable parameters
 		self.batch_size = batch_size
 		self.epochs = epochs
 		self.version = version
 		self.load_model = load_model
+		
+		self.clip_range=(0,255)
 
 		self.dim = (160, 256,1)
 		#size of the test set as a fraction of the total amount of data
@@ -289,6 +296,7 @@ class CGAN(object):
 		#### constant parameters
 		# self.dataset_loc = '../../speed'
 		self.dataset_loc = 'speed/speed/'
+		self.image_root = os.path.join(self.dataset_loc, 'images', 'train')
 #		self.loss_g=keras.losses.mean_squared_error
 #		self.loss_g=keras.losses.logcosh
 		self.loss_g=lambda x,y: -keras.losses.logcosh(x,y)
@@ -311,7 +319,8 @@ class CGAN(object):
 				  'batch_size': self.batch_size,
 				  'latent_dim': self.latent_dim,
 				  'shuffle': True,
-				  'seed': 1}
+				  'seed': 1,
+				  'clip_range': self.clip_range}
 
 		self.dataloader()
 		if self.load_model < 0:
@@ -553,10 +562,27 @@ class CGAN(object):
 			r = np.array([np.random.randn()*2.,np.random.randn()*2.,
 			  np.random.poisson(3.)])
 			pose = np.concatenate([q, r]).reshape(1,7)
-			x = (self.generator.predict([noise,pose])*255).reshape(self.dim[0],self.dim[1])
-			plt.imsave(f'cgantest/TestOutput_{ID}.png', x, cmap = 'gray', dpi = 300)
+			x = ((self.generator.predict([noise,pose]))*(self.clip_range[1]-self.clip_range[0])+self.clip_range[0]).reshape(self.dim[0],self.dim[1])
+#			print(np.amax(x))
+			plt.imsave(f'cgantest/TestOutput_{ID}.png', x, cmap = 'gray', dpi = 300, vmin = 0, vmax = 255)
+			
 			with open(f'cgantest/TestOutput_{ID}.txt', "w") as myfile:
 				myfile.write(str(pose))
+				
+			ID_e='img004844.jpg'
+			img_path = os.path.join(self.image_root, ID_e)
+			img = Image.open(img_path)
+			img = img.resize((self.dim[1],self.dim[0]))
+			img_a = keras_image.img_to_array(img)
+			
+			hist = cv2.calcHist([img_a], [0], None, [256], [0, 256])
+			plt.figure()
+			plt.title("Grayscale Histogram")
+			plt.xlabel("Bins")
+			plt.ylabel("# of Pixels")
+			plt.plot(hist)
+			plt.xlim([0, 50])
+			plt.savefig(f'cgantest/{ID_e}_hist.png')
 				
 	def model_total(self):
 		self.discriminator.trainable=False
@@ -590,7 +616,7 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 # parser.add_argument('--dataset', help='Path to the downloaded speed dataset.', default='')
 parser.add_argument('--epochs', help='Number of epochs for training.', default = 1)
 parser.add_argument('--batch', help='number of samples in a batch.', default = 4)
-parser.add_argument('--version', help='version of the neural network.', default = 18)
+parser.add_argument('--version', help='version of the neural network.', default = 19)
 parser.add_argument('--load', help='load a previously trained network.', default = -1)
 args = parser.parse_args()
 